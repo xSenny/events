ebc2552cdd1168a0488c60df35c2ce26cd51934e
xSenny <ursuarsenie@gmail.com>
2024-02-29 18:45:12 +0200

    webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
-import {createUser, deleteUser, updateUser} from "@/lib/actions/user.action";
 
 export async function POST(req: Request) {
 
